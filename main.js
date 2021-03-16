09e482ad69a9d1fd223c5f69f60869af6992641c
lcijoyce622 <lcijoyce622@gmail.com>
2021-03-16 17:55:14 +0800

    change theme color

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 var skillArray = [
-    { title: 'JAVA', context: '-在校期間使用的語言</br>-學習演算法、資料結構等課程</br>-刷leetcode解題' },
+    { title: 'JAVA', context: '-在校期間使用語言</br>-學習演算法、資料結構等課程</br>-刷leetcode解題' },
     { title: 'Android', context: '-撰寫畢業專題line up</br>-使用語言Java</br>-負責客戶端前端功能撰寫' },
-    { title: 'JavaScript', context: '-撰寫專題山野閃靈</br>-個人履歷介紹網站</br>-負責網頁前端及整合' },
-    { title: 'HTML', context: '-撰寫專題山野閃靈</br>-個人履歷介紹網站' },
+    { title: 'JavaScript', context: '-撰寫專題山野閃靈</br>-負責網頁前端及整合</br>-撰寫個人履歷介紹網站' },
+    { title: 'HTML', context: '-撰寫專題山野閃靈</br>-撰寫個人履歷介紹網站' },
     { title: 'JQuery', context: '-更輕鬆方便的製作網站功能</br>-使用jquery套件' },
     { title: 'MySQL', context: '-撰寫基本語法</br>-學校課程使用</br>-專案使用phpmyadmin建立資料庫' },
     { title: 'PHP', context: '-連結後端資料庫</br>-過濾資料避免瀏覽器負荷過大' },
     { title: 'GIT', context: '-版本控制</br>-Github專案練習' },
     { title: 'BootStrap', context: '-統一網站風格樣式</br>-更加流暢的製作動畫效果</br>-RWD響應式網頁設計' },
-    { title: 'SCSS/CSS', context: '-美化網頁<br>-個人化的設計<br>-簡單動畫特效' },
+    { title: 'SCSS/CSS', context: '-美化網頁<br>-個人化設計<br>-網頁動畫特效' },
     { title: 'Illustrator', context: '-製作wireframe、<br>-設計icon、名片等物件' },
     { title: 'PhotoShop', context: '-matte painting<br>-修圖、繪畫、mockup' }
 ]
